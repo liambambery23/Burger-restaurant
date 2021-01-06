9771e3f954c38419384c2e16abbab56eae52afb5
Liam B <71141670+liambambery23@users.noreply.github.com>
2021-01-06 11:38:27 -0500

    files to create burgers_db and seed it with data

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUE ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUE ('Juicy Lucy');
+INSERT INTO burgers (burger_name) VALUE ('Buffao Chicken Burger');
